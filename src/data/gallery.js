c3874449d4e83824d14c2d21422d225a3c56f2e4
YashGoyal06 <yash.goyal06@proton.me>
2026-05-04 01:37:20 +0530

    Add src/data/gallery.js

---
+++ src/data/gallery.js
@@ -0,0 +1,38 @@
+export const gallery = [
+  {
+    id: 1,
+    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuD5P3tdrl4mWWv74ONcv_gulE-LtAhbQxMzsfmailnAd0n8-Jl4EViEKiaHbLkR5BrrHhFwOKo0o3L9o_eRU_aarQ1mu9zPfUD0dTGca4wGiOJXvorc9HBqVQuQw0DMgffxQiYLv_kqi4Qjkpp_sYOcB9EOAqCBlYJU6K6ZTzBKLUQ2A2zGErxY76fzfKShmwU9gfslrMhnyxwRsiG8kQDPHE8RMQf0zmBzLr6Ibbt6yzIH9zRDwHVamH4hmUjrdUkMPddhGNBC7p87", // Cafe interior
+    category: "Ambience",
+    title: "Cozy Corners"
+  },
+  {
+    id: 2,
+    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuDd97VSX-Q5fstJvda_5sa_DFRcp1P2n0yfAoXtMJJJIXLqnR-6PSrdMiCYkiyEmkA4MY4elLVmL0T4sktgQJZoew5j5QjKJQMcGbwMVXvpZYNKC7RETdN0QYMrVfyosSm4x4CV0eaELDo9CS8viuBuQF45Don8OPu5vT4zTd8HUYnbO3ge25TdToEMXeuIyaQwzBuNBXpzks3GneNh350d1vYjh12ntv6xPM8326lk08c4zIQ3LeQOnLQuese2Cz9ksUj2ML487TY0", // Signature Mocha
+    category: "Coffee",
+    title: "Latte Art"
+  },
+  {
+    id: 3,
+    image: "https://images.unsplash.com/photo-1554118811-1e0d58224f24?auto=format&fit=crop&w=800&q=80",
+    category: "Ambience",
+    title: "Modern Aesthetics"
+  },
+  {
+    id: 4,
+    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuDAp_0VqncCBr0sMIb60OrFqUJ0Pux-L-rI281VnPczyZLoRXYlrqdyj_zWbzRfbLzhDEz5JGmagsKGAi-B_qsdW-kPmouRS23cklOsnhlsorv3tEz1kNhh_cpgTUugzeBE-1EcSjDkT3wi9IYPuH-WQApSn3pj7DD8slS-ANCshA-PWDhjStU0lJwX40SWl54cEa0A3smAkPyBFj66e_L66XXXwd3ZIS0QYoUK-6mE41v2PZsBP0BeT0MwOehtEZn6TYQxrzViVLfE", // Avocado Toast
+    category: "Food",
+    title: "Fresh Bites"
+  },
+  {
+    id: 5,
+    image: "/good_times.png",
+    category: "People",
+    title: "Good Times"
+  },
+  {
+    id: 6,
+    image: "https://images.unsplash.com/photo-1497935586351-b67a49e012bf?auto=format&fit=crop&w=800&q=80",
+    category: "Coffee",
+    title: "Perfect Pour"
+  }
+];
